3858563431eba5f47fd10b9018823bab4065f2be
sph-huynh <shuy14@student.monash.edu>
2018-03-21 17:42:22 +1100

    Minor changes and update in some content

--- static/js/slideshow.js
+++ static/js/slideshow.js
@@ -11,5 +11,5 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1} 
     slides[slideIndex-1].style.display = "block"; 
-    setTimeout(showSlides, 5000); // Change image every 3 seconds
+    setTimeout(showSlides, 9000); // Change image every 3 seconds
 }
